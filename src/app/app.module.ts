506613296874e4da45b03324c36570939d150b92
Andre Santos <andredupdm@gmail.com>
2023-04-30 16:38:54 -0300

    fix: change path for navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './view/home/home.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { NavbarComponent } from './components/layout/navbar/navbar.component';
 import { ContainerComponent } from './components/layout/container/container.component';
 import { DescriptionComponent } from './components/description/description.component';
 import { TableComponent } from './components/table/table.component';
